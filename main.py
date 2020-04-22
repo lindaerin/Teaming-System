bd150265777c074d3b14e7ddce41ad42ec4fbeda
lindaerin <lindaerin00@gmail.com>
2020-04-22 03:43:54 -0400

    added invite feature and db functionality, structure and more readable code changes

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.secret_key = '111'
 # Enter your database connection details below
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '111111'
+app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'csc322_project'
 
 # Intialize MySQL
@@ -241,19 +241,46 @@ def profile():
         # Fetch all records and return result
         post_history = cursor.fetchall()
         # get all the groups' information that the user
-        cursor.execute('SELECT tb_group.*, tb_group_members.user_name FROM tb_group_members INNER JOIN tb_group'
-                       ' ON tb_group.group_id = tb_group_members.group_id AND tb_group_members.user_name = %s',
-                       [session['username']])
+        cursor.execute('SELECT tb_group.*, tb_group_members.user_id FROM tb_group_members INNER JOIN tb_group'
+                       ' ON tb_group.group_id = tb_group_members.group_id AND tb_group_members.user_id = %s',
+                       [session['user_id']])
         group_info = cursor.fetchall()
         # Show the profile page with account info
         return render_template('profile.html', account=account, post_history=post_history, group_info=group_info)
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
 
+# public user profile
+@app.route('/user/<user_name>')
+def public_profile(user_name):
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    # cursor.execute('SELECT * FROM tb_user WHERE user_name = %s', (user_name,))
+
+
+
+    # join table user and table profile to get the poster information: id, name, email, user_type, user_scores
+    cursor.execute('SELECT tb_profile.*, tb_user.user_name, tb_user.email, tb_user.user_id'
+                   ' FROM tb_user INNER JOIN tb_profile ON'
+                   ' tb_profile.user_id = tb_user.user_id WHERE tb_user.user_name = %s', (user_name,))
+    account = cursor.fetchone()
+    print(account)
+
+
+    # if no username exists on file, return error
+    if not account:
+        flash("User doesn't exist")
+        return redirect(url_for('404'))
+
+    # get user's post history information: id, title, author, content, post_time and order by desc
+    cursor.execute('SELECT tb_post.*, tb_user.user_id FROM tb_post INNER JOIN tb_user ON'
+                       ' tb_post.user_id = tb_user.user_id WHERE tb_post.user_id = % s order by -post_time',
+                       (account['user_id'],))
+    post_history = cursor.fetchall()
+
+    return render_template('profile.html', public_profile=account, post_history=post_history)
 
 # this will be the poster_file page
 @app.route('/poster_profile/<poster_id>')
-@login_required
 def poster_profile(poster_id):
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     # join table user and table profile to get the poster information: id, name, email, user_type, user_scores
@@ -315,7 +342,6 @@ def post():
 
 # search bar
 @app.route('/search/', methods=['GET', 'POST'])
-@login_required
 def search():
     if request.method == "POST" and 'username' in request.form:
         username = request.form['username']
@@ -327,55 +353,156 @@ def search():
             flash('User does not exist')
             return redirect(url_for('home'))
         # if user search himself, it will into his profile page directly
-        elif account['user_name'] == session['username']:
-            return redirect(url_for('profile'))
+        # elif account['user_name'] == session['username']:
+        #     return redirect(url_for('profile'))
         else:
             # otherwise, go to the profile page of the searched user
-            return redirect(url_for('poster_profile', poster_id=account['user_id']))
+            return redirect(url_for('public_profile', user_name=account['user_name']))
 
 
 # create a group
+# @app.route('/group/', methods=['GET', 'POST'])
+# # def create_group():
+# #     if request.method == "POST":
+# #         group_name = request.form['group_name']
+# #         group_describe = request.form['describe']
+# #         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+# #         # check if the group name entered by the user was in the table group
+# #         cursor.execute('SELECT * FROM tb_group WHERE group_name = %s', (group_name,))
+# #         group_name_exist = cursor.fetchone()
+# #         # if exist: show the error message and return to the profile page
+# #         if group_name_exist:
+# #             flash('Group Already Exist')
+# #             return redirect(url_for('profile'))
+# #         # otherwise insert data into table group: group_name, user_id, group_describe
+# #         cursor.execute('INSERT INTO tb_group (group_name, user_id, group_describe) VALUES (%s, %s, %s)',
+# #                        (group_name, session['user_id'], group_describe))
+# #         mysql.connection.commit()
+# #         # get the group id by desc
+# #         cursor.execute('SELECT group_id FROM tb_group order by -group_id')
+# #         group_id = cursor.fetchone()
+# #         # insert data into table group_members: group_id and user_name
+# #         cursor.execute('INSERT INTO tb_group_members (group_id, user_name) VALUES (%s, %s)',
+# #                        (group_id['group_id'], session['username']))
+# #         mysql.connection.commit()
+# #         return redirect(url_for('profile'))
+
 @app.route('/group/', methods=['GET', 'POST'])
 def create_group():
-    if request.method == "POST":
-        group_name = request.form['group_name']
-        group_describe = request.form['describe']
+    msg = ''
+    # if request.method == 'POST' and 'team_name' in request.form and 'invite' in request.form:
+    if request.method == 'POST':
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        # check if the group name entered by the user was in the table group
+        # Create variables for easy access
+        group_name = request.form['group_name']
+        invite = request.form['invite']
+        description = request.form['description']
+
+        # convert team member user_names into string of user_id to pass into stored procedure
+        # add session['user_id']
+        group_members = str(session['user_id'])
+
+        new_invite = invite.split(",")
+
+        # take array of split user_names and convert into user_id string
+        for i in range(0, len(new_invite)):
+            cursor.execute('SELECT user_id from tb_user WHERE user_name = %s', (new_invite[i],))
+            current = cursor.fetchone()
+            current_user = current['user_id']
+            group_members = group_members + "," + str(current_user)
+
+        if not group_name:
+            msg='Please enter a group name'
+            return render_template('group.html', msg=msg, group_name=group_name, invite=invite, description=description)
+
+        if not invite:
+            msg = 'Please invite users to group'
+            return render_template('group.html', msg=msg, group_name=group_name, invite=invite, description=description)
+
+        if not invite and not group_name:
+            msg = 'Please fill out the form'
+            return render_template('group.html', msg=msg)
+
+        # cursor.execute('SELECT user_id from tb_user WHERE user_name = %s', (invite,))
+        # invite_id = cursor.fetchone()
+        # invite_id_final = invite_id['user_id']
+
+        # Check if team_name exists in tb_group table
         cursor.execute('SELECT * FROM tb_group WHERE group_name = %s', (group_name,))
-        group_name_exist = cursor.fetchone()
-        # if exist: show the error message and return to the profile page
-        if group_name_exist:
-            flash('Group Already Exist')
-            return redirect(url_for('profile'))
-        # otherwise insert data into table group: group_name, user_id, group_describe
-        cursor.execute('INSERT INTO tb_group (group_name, user_id, group_describe) VALUES (%s, %s, %s)',
-                       (group_name, session['user_id'], group_describe))
-        mysql.connection.commit()
-        # get the group id by desc
-        cursor.execute('SELECT group_id FROM tb_group order by -group_id')
-        group_id = cursor.fetchone()
-        # insert data into table group_members: group_id and user_name
-        cursor.execute('INSERT INTO tb_group_members (group_id, user_name) VALUES (%s, %s)',
-                       (group_id['group_id'], session['username']))
-        mysql.connection.commit()
-        return redirect(url_for('profile'))
+        name = cursor.fetchone()
+
+        # If team_name currently exists, show error that it is taken
+        if name:
+            msg = 'This Project Name is already taken!'
+            return render_template('group.html', msg=msg, group_name=group_name, invite=invite, description=description)
+        else:
+            msg = 'You have successfully created a team!'
+            # Insert project name to make project id into tb_group
+            cursor.execute("INSERT INTO tb_group (group_name, group_describe)"
+                           " VALUES (%s, %s)", (group_name, description,))
+            # Get newly created group_id
+            cursor.execute('SELECT group_id FROM tb_group WHERE group_name = %s', (group_name,))
+            new_team = cursor.fetchone()
+            new_team_val = new_team['group_id']
+
+            # Run Stored Procedure to run on invited members and add to tb_group_members
+            cursor.callproc('insert_members', [group_members, new_team_val])
+
+            # Save new data into database
+            mysql.connection.commit()
+            # session['group_members'] = invite
+
+            return redirect(url_for('group_page', group_name=group_name))
+
+    return render_template('group.html')
 
 
-# group page
-@app.route("/into_group/<group_id>")
-def into_group(group_id):
+
+
+@app.route('/group/<group_name>', methods=['GET', 'POST'])
+def group_page(group_name):
+    # check if group name exists within database
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    # get the group's information: group_name, group_describe, group_id, group_created_time, group_creater if exist
-    cursor.execute('SELECT tb_group.*, tb_group_members.user_name FROM tb_group_members INNER JOIN tb_group'
-                   ' ON tb_group.group_id = %s AND tb_group_members.user_name = %s', (group_id, session['username']))
-    group = cursor.fetchone()
-    # get the group members' information: user_name, user_id
-    cursor.execute('SELECT tb_group_members.*, tb_user.user_id FROM tb_group_members INNER JOIN tb_user '
-                   'ON tb_group_members.user_name = tb_user.user_name WHERE'
-                   ' tb_group_members.group_id = %s', (group_id,))
-    group_members = cursor.fetchall()
-    return render_template('group.html', group=group, group_members=group_members)
+    cursor.execute('SELECT group_name from tb_group WHERE group_name = %s', (group_name,))
+    groupExists = cursor.fetchone()
+    # app.logger.info(groupExists)
+
+    if groupExists:
+        # get group_id
+        cursor.execute('SELECT * from tb_group WHERE group_name = %s', (group_name,))
+        group = cursor.fetchone()
+        group_id = group['group_id']
+
+        # get list of users from table and save it as a string
+        # INNER JOIN HERE
+        cursor.execute('SELECT user_name from tb_user inner join tb_group_members ON tb_user.user_id = tb_group_members.user_id WHERE group_id = %s', (group_id,))
+        usernames = cursor.fetchall()
+
+        final_usernames = usernames[0]['user_name']
+
+        # loop through each usernames tuple and add them to final_usernames string
+        for i in range(1, len(usernames)):
+            current_user = usernames[i]['user_name']
+            final_usernames = final_usernames + ", " + current_user
+
+        print(final_usernames)
+
+        # get group description
+        cursor.execute('SELECT group_describe from tb_group WHERE group_name = %s', (group_name,))
+        desc = cursor.fetchone()
+        team_desc = desc['group_describe']
+
+        print(group)
+
+        return render_template('group_page.html', group=group, members=final_usernames, description=team_desc)
+    else:
+        return render_template('404.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html')
+
 
 
 # invite feature
@@ -390,7 +517,7 @@ def invite(group_id):
         # if not exist, output error message, then return to the group page
         if not user_name_exist:
             flash("User doesn't exist")
-            return redirect(url_for('into_group', group_id=group_id))
+            return redirect(url_for('group_page', group_id=group_id))
         # otherwise, check the username if exist in this group
         cursor.execute('SELECT tb_group_members.* FROM tb_group_members INNER JOIN tb_user ON '
                        'tb_group_members.user_name = tb_user.user_name'
@@ -399,11 +526,11 @@ def invite(group_id):
         # if this user already in this group, show the error message
         if group_member:
             flash('This User Already in this Group')
-            return redirect(url_for('into_group', group_id=group_id))
+            return redirect(url_for('group_page', group_id=group_id))
         # otherwise, insert data into table group_members
         cursor.execute('INSERT INTO tb_group_members (group_id, user_name) VALUES (%s, %s)', (group_id, user_name))
         mysql.connection.commit()
-        return redirect(url_for('into_group', group_id=group_id))
+        return redirect(url_for('group_page', group_id=group_id))
 
 
 if __name__ == '__main__':
