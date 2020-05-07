27101de8331e0232b8f2a0e87645a42b13abf8a3
Masuda50 <mfarehia@gmail.com>
2020-05-07 18:38:27 -0400

    The entire invite/blacklist/white list functionality - also made usernames unique to each user

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ mail_settings = {
     "MAIL_SUPPRESS_SEND": False
 }
 
+#creates the mail feature 
 app.config.update(mail_settings)
 mail = Mail(app)
 
@@ -31,7 +32,7 @@ app.secret_key = '111'
 # Enter your database connection details below
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '111111'
+app.config['MYSQL_PASSWORD'] = 'Bang1adesh'
 app.config['MYSQL_DB'] = 'csc322_project'
 
 # Intialize MySQL
@@ -61,7 +62,6 @@ def admin_login_required(func):  # admin login required decorato
 
     return wrapper
 
-
 @app.route("/admin", methods=['get', 'post'])  # admin adding or deleting accounts
 @admin_login_required #must be logged in as admin to access this page!
 def admin():
@@ -75,13 +75,14 @@ def admin():
             credential = request.form['credential']
             user_password = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(10)])
             cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-            #check to see if accoutn already exist 
-            cursor.execute('SELECT * FROM tb_user WHERE email = %s', (email,))
+            #check to see if account already exist by email or user_name
+            cursor.execute('SELECT * FROM tb_user WHERE email = %s OR user_name = %s', (email,user_name))
             account = cursor.fetchone()
             #if it doesnt insert into db 
             if not account:
                 cursor.execute("INSERT INTO tb_user (user_name, user_password, email, credential, interest)"
                                " VALUES (%s, %s, %s, %s, %s)", (user_name, user_password, email, credential, interest))
+                #send email depeding if it is an appeal or first time application 
                 if request.form['message'] == "NONE":
                     welcome = user_name + " your Whiteboard application has been approved and your account has been created. Your temporary password is " + user_password + ". Use this link to reset your password : http://localhost:5000/reset_password"
                 else: 
@@ -98,12 +99,12 @@ def admin():
             username = request.form['username']
             email = request.form['email']
 
-            #send email
+            #send email depending if it appeal or first time applications 
             if request.form['message'] == "NONE":
                 reject = username + " we are sorry to say, your Whiteboard application has not been approved. Click on this link in order to appeal: http://localhost:5000/appeal"
             else:
                 reject = username + " We are sorry to say, your Whiteboard appeal has not been approved"
-                #put them into the blacklist 
+                #put them into the blacklist if it is an appeal 
                 cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
                 cursor.execute("INSERT INTO tb_blacklist (email)" "VALUES (%s)", (email,))
                 mysql.connection.commit()
@@ -126,6 +127,7 @@ def admin():
     return render_template('admin.html')
 
 
+
 #page where they can reset the password 
 @app.route('/reset_password', methods=['POST', 'GET'])
 def reset_password():
@@ -136,11 +138,13 @@ def reset_password():
         old_password = request.form['old_password']
         new_password = request.form['new_password']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        #check to see if the account exists and the information is correct 
         cursor.execute('SELECT * FROM tb_user WHERE email = %s and user_password = %s' , (email, old_password))
         account = cursor.fetchone()
         if not account:
             msg = "Unable to reset password"
             return render_template('reset_password.html',msg=msg)
+        #if the info is correct than change password and indicate they have changed their password 
         else:
             msg = "Success!"
             cursor.execute('UPDATE tb_user SET user_password = %s, didtheychangepass = %s WHERE user_id = %s' , (new_password, '1' , account['user_id']))
@@ -160,14 +164,16 @@ def appeal():
         reference = request.form['reference']
         message = request.form['message']
 
+        #check to see if the email is already in the db 
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM tb_user WHERE email = %s', (email,))
+        cursor.execute('SELECT * FROM tb_user WHERE email = %s OR user_name = %s' , (email,username))
         account = cursor.fetchone()
-        cursor.execute('SELECT * FROM tb_applied WHERE email = %s',(email,))
+        cursor.execute('SELECT * FROM tb_applied WHERE email = %s OR username = %s',(email,username))
         applied = cursor.fetchone()
 
+        #if email is in the system they cannot appeal 
         if account or applied:
-            msg = "You are already in the system - please check your email for a message from Whiteboard"
+            msg = "You are already in the system - please check your email for a message from Whiteboard or try a different username"
             return render_template("appeal.html", msg=msg)
         else:
             cursor.execute("INSERT INTO tb_applied (username, email, interest, credential, reference, message)"
@@ -299,13 +305,13 @@ def register():
 
         # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM tb_user WHERE email = %s', (email,))
+        cursor.execute('SELECT * FROM tb_user WHERE email = %s OR user_name = %s', (email,username))
         account = cursor.fetchone()
-        cursor.execute('SELECT * FROM tb_applied WHERE email = %s', (email,))
+        cursor.execute('SELECT * FROM tb_applied WHERE email = %s OR username = %s', (email,username))
         application = cursor.fetchone()
         # If account doesnt exists show error and validation checks
         if account or application:
-            msg = 'Invalid Email!'
+            msg = 'Invalid Email or Username!'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
             msg = 'Invalid email address!'
         elif not re.match(r'[A-Za-z]+', username):
@@ -347,10 +353,77 @@ def my_context_processor():
 
 
 # this will be the profile page, only accessible for loggedin users
-@app.route('/profile/myProfile')
+@app.route('/profile/myProfile', methods=['POST', 'GET'] )
 def profile():
     # Check if user is loggedin
     if 'loggedin' in session:
+        if request.method == "POST":
+
+            #is user approved a group invitations 
+            if 'Approve' in request.form:
+                group_id = request.form['group_id']
+                cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+                #insert them into the group, insert a message into the group chat, and delete the group from invites 
+                cursor.execute('INSERT INTO tb_group_members (group_id, user_name) VALUES (%s, %s)', (group_id, [session['username']]))
+                cursor.execute('INSERT INTO tb_chat (user_id, group_id, chat_content) VALUES (%s, %s, %s)',
+                       ([session['user_id']], group_id, "Has joined the group!"))
+                cursor.execute('DELETE from tb_invite WHERE user_id = %s AND group_id = %s', ([session['user_id']],group_id))
+                mysql.connection.commit()
+
+            #if user reject an invite 
+            elif 'Reject' in request.form: 
+                rejection = request.form['rejection']
+                message = "Thank you for the consideration, but I will not accept invitation to this group. I cannot join this group, " + rejection
+                group_id = request.form['group_id']
+                #insert the rejection message into group chat and delete the invitation 
+                cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+                cursor.execute('INSERT INTO tb_chat (user_id, group_id, chat_content) VALUES (%s, %s, %s)',
+                       ([session['user_id']], group_id, message))
+                cursor.execute('DELETE from tb_invite WHERE user_id = %s AND group_id = %s', ([session['user_id']],group_id))
+                mysql.connection.commit()
+
+            #if user is adding a user to their whitelist 
+            elif "whitelist" in request.form:
+                user_whitelist = request.form['user_whitelist']
+                cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+                # check the user name the user inputted if exist in user database
+                cursor.execute('SELECT user_name FROM tb_user WHERE user_name = %s', (user_whitelist,))
+                user_name_exist = cursor.fetchone()
+                # if not exist, output error message, then return to the group page
+                if not user_name_exist:
+                    flash("User doesn't exist")
+                else: 
+                    #else check if the user is already in their whitelist or blacklist
+                    cursor.execute('SELECT * FROM tb_whitelist WHERE user_id = %s AND user_name_friend = %s', ([session['user_id']],user_whitelist))
+                    exist = cursor.fetchone()
+                    cursor.execute('SELECT * FROM tb_user_blacklist WHERE user_id = %s AND user_name_blocked = %s', ([session['user_id']],user_whitelist))
+                    otherlist = cursor.fetchone()
+                    #if not then insert it into the correct list 
+                    if not exist and not otherlist:
+                        cursor.execute("INSERT INTO tb_whitelist (user_id, user_name_friend)" "VALUES (%s,%s)", ([session['user_id']],user_whitelist)) 
+                        mysql.connection.commit()
+
+            #user adding into the black list 
+            elif "blacklist" in request.form:
+                user_blacklist = request.form['user_blacklist']
+                cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+                # check the user name the user inputted if exist in user database
+                cursor.execute('SELECT user_name FROM tb_user WHERE user_name = %s', (user_blacklist,))
+                user_name_exist = cursor.fetchone()
+                # if not exist, output error message, then return to the group page
+                if not user_name_exist:
+                    flash("User doesn't exist")
+                else: 
+                    #check if the user already exist in either of list 
+                    cursor.execute('SELECT * FROM tb_user_blacklist WHERE user_id = %s AND user_name_blocked = %s', ([session['user_id']],user_blacklist))
+                    exist = cursor.fetchone()
+                    cursor.execute('SELECT * FROM tb_whitelist WHERE user_id = %s AND user_name_friend = %s', ([session['user_id']],user_blacklist))
+                    otherlist = cursor.fetchone()
+                    #if not put them into the list 
+                    if not exist and not otherlist:
+                        cursor.execute("INSERT INTO tb_user_blacklist (user_id, user_name_blocked)" "VALUES (%s,%s)", ([session['user_id']],user_blacklist)) 
+                        mysql.connection.commit()  
+
         # We need all the account info for the user so we can display it on the profile page
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         # join table profile and table user to get user information: id, name, email, user_type, user_scores,
@@ -367,8 +440,16 @@ def profile():
                        ' ON tb_group.group_id = tb_group_members.group_id AND tb_group_members.user_name = %s',
                        [session['username']])
         group_info = cursor.fetchall()
+        #have to join with the group tablle 
+        cursor.execute('SELECT tb_invite.*, tb_group.group_name, tb_group.group_describe FROM tb_group INNER JOIN tb_invite ON tb_invite.group_id = tb_group.group_id WHERE tb_invite.user_id = %s', [session['user_id']])
+        invitation = cursor.fetchall()
+        #get black and whitelist informations 
+        cursor.execute('SELECT user_name_friend FROM tb_whitelist WHERE user_id = %s', [session['user_id']])
+        friends = cursor.fetchall()
+        cursor.execute('SELECT user_name_blocked FROM tb_user_blacklist WHERE user_id = %s', [session['user_id']])
+        blocked = cursor.fetchall()
         # Show the profile page with account info
-        return render_template('profile.html', account=account, post_history=post_history, group_info=group_info)
+        return render_template('profile.html', account=account, post_history=post_history, group_info=group_info, invitation=invitation, friends=friends, blocked=blocked)
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
 
@@ -564,9 +645,47 @@ def invite(group_id):
         if group_member:
             flash('This User Already in this Group')
             return redirect(url_for('into_group', group_id=group_id))
-        # otherwise, insert data into table group_members
-        cursor.execute('INSERT INTO tb_group_members (group_id, user_name) VALUES (%s, %s)', (group_id, user_name))
-        mysql.connection.commit()
+            
+        # otherwise, see where to place the user by getting their username 
+        cursor.execute('SELECT user_id FROM tb_user WHERE user_name = %s', (user_name,))
+        account = cursor.fetchone();
+        where_to_place = 'invitation'
+
+        #check to see if the adder is in the invitees white or black list 
+        cursor.execute("SELECT user_name_friend FROM tb_whitelist WHERE user_id = %s", ([account['user_id']],))
+        friends = cursor.fetchall()  
+        cursor.execute("SELECT user_name_blocked FROM tb_user_blacklist WHERE user_id =%s", ([account['user_id']]))
+        blocked = cursor.fetchall()
+        for friend in friends:
+            if friend['user_name_friend'] == session['username']:
+                where_to_place = "whitelist"
+        for block in blocked:
+            if block['user_name_blocked'] == session['username']:
+                where_to_place = 'blacklist'
+
+        #added in in invitee whitelist automatically add into the group with a message 
+        if where_to_place == 'whitelist':
+            cursor.execute('INSERT INTO tb_group_members (group_id, user_name) VALUES (%s, %s)', (group_id, user_name))
+            cursor.execute('INSERT INTO tb_chat (user_id, group_id, chat_content) VALUES (%s, %s, %s)',
+                    ([account['user_id']], group_id, "Has joined the group!"))
+            mysql.connection.commit()
+
+        #adder is in the blacklist display automatic rejection message in group 
+        elif where_to_place == 'blacklist':
+            cursor.execute('INSERT INTO tb_chat (user_id, group_id, chat_content) VALUES (%s, %s, %s)',
+                    ([account['user_id']], group_id, "Thank you for the consideration, but I will not accept invitation to this group."))
+            mysql.connection.commit()
+
+        #adder is in neither, send a invitation to the invitee's profile page   
+        else: 
+            cursor.execute("SELECT * FROM tb_invite WHERE user_id =%s AND group_id = %s", (account['user_id'],group_id))
+            invited = cursor.fetchone()
+            #check to see if invitations already sent
+            if invited:
+                flash('This users is already invited')
+            else:
+                cursor.execute("INSERT INTO tb_invite (user_id,group_id)" " VALUES (%s,%s)", (account['user_id'],group_id))
+                mysql.connection.commit()
         return redirect(url_for('into_group', group_id=group_id))
 
 
@@ -629,6 +748,11 @@ def poll_vote(group_id):
             mysql.connection.commit()
 
             return redirect(url_for('into_group', group_id=group_id))
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html')
 
 
 if __name__ == '__main__':
